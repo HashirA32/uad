c6af565bf0d3902d4da3fd7d5542d33509f6283c
HashirA32 <hashirofficiala32@gmail.com>
2024-06-27 23:34:45 +0500

    ACADEMIC PROGRAMS

--- src/comps/Acadamic.js
+++ src/comps/Acadamic.js
@@ -6,21 +6,101 @@ const Acadamic = () => {
     <>
      <div className='ACADEMIC'>
         <p className='Academic-Heading'>ACADEMIC PROGRAMS</p>
+        <p className='Academic-text'>EXPLORE UNIVERSITY’S 10+ COURSES ACROSS VARIOUS SPECIALIZATIONS THAT PROMOTE INTELLECTUAL AND INTUITIVE LEARNING AMONG STUDENTS.</p>
 
         <div className='cards-Contianer'>
+
         <div className="card">
-          <img src="./images/Background-image.jpg" alt="Card" className="card__image" />
+          <img src="./images/file.png" alt="Card" className="card__image" />
           <div className="card__content">
             <p className="card__title">BS Computer Science</p>
-            <p className="card__description">Explore BS in Computer Science at Agriculture University of Dera Ismail Khan (AUP). Gain programming and IT expertise for tech careers. Discover modern facilities and experienced faculty.</p>
+            <p className="card__description">Explore BS in Computer Science at Agriculture University of Dera Ismail Khan (UAD). Gain programming and IT expertise for tech careers. Discover modern facilities and experienced faculty.</p>
           </div>
         </div>
 
-        
-       
+        <div className="card">
+          <img src="./images/Agriculture.png" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Agriculture</p>
+            <p className="card__description">Explore BS in Agriculture at Agriculture University of Dera Ismail Khan (UAD). Gain expertise in agricultural sciences and sustainable farming, with modern facilities and experienced faculty.</p>
+          </div>
+        </div>
+
+
+        <div className="card">
+          <img src="./images/English.png" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS English</p>
+            <p className="card__description">Discover BS in English at Agriculture University of Dera Ismail Khan (UAD). Enhance your skills in literature, linguistics, and communication with expert faculty and modern resources.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./images/Forstory.jpg" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Forestory</p>
+            <p className="card__description">Discover BS in Forestry at Agriculture University of Dera Ismail Khan (UAD). Learn sustainable forest management and conservation techniques with top-notch facilities and expert faculty.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./images/Math.png" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Math</p>
+            <p className="card__description">Explore BS in Mathematics at Agriculture University of Dera Ismail Khan (UAD). Master analytical techniques and mathematical theories with dedicated faculty and state-of-the-art facilities.</p>
+          </div>
+        </div>
+
+
+        <div className="card">
+          <img src="./images/Dental.jpg" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Dental Technology </p>
+            <p className="card__description">Pursue a BS in Dental Technology at Agriculture University of Dera Ismail Khan (UAD). Acquire practical skills in dental science and technology with experienced instructors and modern laboratories.</p>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+        <div className="card">
+          <img src="./images/Zoology.jpg" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Zoology</p>
+            <p className="card__description">Enroll in BS Zoology at Agriculture University of Dera Ismail Khan (UAD). Study animal biology and ecosystems with comprehensive coursework and expert faculty.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./images/Chemistry.jpg" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Chemistry</p>
+            <p className="card__description">Embark on a journey in Chemistry with a BS degree from Agriculture University of DI Khan (UAD). Explore chemical sciences, laboratory research, and innovative applications. Start your career in this fundamental scientific field today!</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="./images/Radialogy.jpg" alt="Card" className="card__image" />
+          <div className="card__content">
+            <p className="card__title">BS Radiology Technology</p>
+            <p className="card__description">Discover Radiology Technology with a BS degree from Agriculture University of DI Khan (UAD). Explore medical imaging, patient care, and advanced diagnostics. Begin your journey in this essential healthcare field today!</p>
+          </div>
+        </div>
+
+
+
+
 
         </div>
       </div> 
+      
     </>
   )
 }
