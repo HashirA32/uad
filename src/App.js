c2df8fa214278c41dfb0ed8302554ea66a8535f8
HashirA32 <hashirofficiala32@gmail.com>
2024-07-19 10:51:30 -0700

    Counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Navbar from './comps/Navbar';
 import './App.css';
 import Hero from './comps/Hero';
 import Acadamic from './comps/Acadamic';
+import Faculty from './comps/Faculty';
+import Counter from './comps/Counter';
+import Feedback from './comps/Feedback'
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
     <Acadamic/>
+    <Faculty/>
+    <Counter/>
+    <Feedback/>
+
     </> 
   );
 }
